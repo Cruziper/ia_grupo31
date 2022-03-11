1c008c85f5ec20e4f0b77fd53cd3a7304287e390
Cruziper <goncalo.garcia.cruz@tecnico.ulisboa.pt>
2022-03-11 18:19:16 +0000

    Implemented adjacent_vertical_numbers (#10, closes #2)

--- numbrix.py
+++ numbrix.py
@@ -35,8 +35,14 @@ class Board:
     def adjacent_vertical_numbers(self, row: int, col: int) -> (int, int):
         """ Devolve os valores imediatamente abaixo e acima, 
         respectivamente. """
-        # TODO
-        pass
+        if (row-1 >= 1):
+            if(row+1 <= int(self[0][0])):
+                return (int(self[row+1][col-1]), int(self[row-1][col-1]))
+            else:
+                return (-1, int(self[row-1][col-1]))
+        else:
+            if(row+1 <= int(self[0][0])):
+                return (int(self[row+1][col-1]), -1)
     
     def adjacent_horizontal_numbers(self, row: int, col: int) -> (int, int):
         """ Devolve os valores imediatamente à esquerda e à direita, 
@@ -99,6 +105,8 @@ if __name__ == "__main__":
     # Ler o ficheiro de input de sys.argv[1],
     board = Board.parse_instance(sys.argv[1])
     print(board)
+    number = Board.adjacent_vertical_numbers(board, 1, 3)
+    print(number)
     # Usar uma técnica de procura para resolver a instância,
     # Retirar a solução a partir do nó resultante,
     # Imprimir para o standard output no formato indicado.

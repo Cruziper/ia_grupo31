08b68c317ed0564cf26b61701604d78037c25bd8
Cruziper <goncalo.garcia.cruz@tecnico.ulisboa.pt>
2022-04-02 17:29:41 +0100

    Code Refactor, after teacher input

--- numbrix.py
+++ numbrix.py
@@ -2,14 +2,16 @@
 # Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
 # Além das funções e classes já definidas, podem acrescentar outras que considerem pertinentes.
 
-# Grupo 00:
-# 00000 Nome1
-# 00000 Nome2
+# Grupo 31:
+# 84721 Gonçalo Cruz
+# 98789 António Cunha
 
 import sys
 from search import Problem, Node, astar_search, breadth_first_tree_search, depth_first_tree_search, greedy_search, recursive_best_first_search
 
-
+###################################################################################################
+#                                         NUMBRIXSTATE                                            #
+###################################################################################################
 class NumbrixState:
     state_id = 0
 
@@ -24,36 +26,52 @@ class NumbrixState:
     # TODO: outros metodos da classe
 
 
+###################################################################################################
+#                                             BOARD                                               #
+###################################################################################################
 class Board:
+    boardSize = 0
     """ Representação interna de um tabuleiro de Numbrix. """
     
     def get_number(self, row: int, col: int) -> int:
         """ Devolve o valor na respetiva posição do tabuleiro. """
-        return self[row][col-1]
+        return self[Board.findRow(row)][Board.findColumn(col)]
     
     def adjacent_vertical_numbers(self, row: int, col: int):
         """ Devolve os valores imediatamente abaixo e acima, 
         respectivamente. """
         if (row-1 >= 1):
-            if(row+1 <= int(self[0][0])):
-                return (int(self[row+1][col-1]), int(self[row-1][col-1]))
+            if(row+1 <= Board.boardSize):
+                """ Se tiver casa imediatamente abaixo e acima
+                Return: ([linha+1][coluna], [linha-1][coluna]) """
+                return (int(self[Board.findRow(row+1)][Board.findColumn(col)]), int(self[Board.findRow(row-1)][Board.findColumn(col)]))
             else:
-                return (-1, int(self[row-1][col-1]))
+                """ Se tiver apenas casa imediatamente acima
+                Return: (None, [linha-1][coluna]) """
+                return (None, int(self[Board.findRow(row-1)][Board.findColumn(col)]))
         else:
-            if(row+1 <= int(self[0][0])):
-                return (int(self[row+1][col-1]), -1)
+            if(row+1 <= Board.boardSize):
+                """ Se tiver apenas casa imediatamente abaixo
+                Return: ([linha+1][coluna], None) """
+                return (int(self[Board.findRow(row+1)][Board.findColumn(col)]), None)
     
     def adjacent_horizontal_numbers(self, row: int, col: int):
         """ Devolve os valores imediatamente à esquerda e à direita, 
         respectivamente. """
-        if (col-2 >= 1):
-            if(col <= int(self[0][0])):
-                return (int(self[row][col-2]), int(self[row][col]))
+        if (col-1 >= 1):
+            if(col+1 <= Board.boardSize):
+                """ Se tiver casa imediatamente à esquerda e à direita
+                Return: ([linha][coluna-1], [linha][coluna+1]) """
+                return (int(self[Board.findRow(row)][Board.findColumn(col-1)]), int(self[Board.findRow(row)][Board.findColumn(col+1)]))
             else:
-                return (-1, int(self[row][col]))
+                """ Se tiver apenas casa imediatamente à esquerda
+                Return: ([linha][coluna-1], None) """
+                return (int(self[Board.findRow(row)][Board.findColumn(col-1)]), None)
         else:
-            if(col <= int(self[0][0])):
-                return (int(self[row][col-2]), -1)
+            if(col <= Board.boardSize):
+                """ Se tiver apenas casa imediatamente à direita
+                Return: (None, [linha][coluna+1]) """
+                return (None, int(self[Board.findRow(row)][Board.findColumn(col+1)]))
     
     @staticmethod    
     def parse_instance(filename: str):
@@ -61,15 +79,25 @@ class Board:
         uma instância da classe Board. """
         board = []
         with open(filename, "r") as f:
+            # index 0 --> N
+            Board.boardSize = int(f.readline())
             for line in f:
-                # index 0 --> N
                 row = [column.strip() for column in line.split('\t')]
                 board.append(row)
+                    
         return board
 
-    # TODO: outros metodos da classe
+    def findRow(row: int):
+        """ devolve o valor do index da linha """
+        return row-1
+    
+    def findColumn(col: int):
+        """ devolve o valor do index da coluna """
+        return col-1
 
-
+###################################################################################################
+#                                            NUMBRIX                                              #
+###################################################################################################
 class Numbrix(Problem):
     def __init__(self, board: Board):
         """ O construtor especifica o estado inicial. """
@@ -105,14 +133,23 @@ class Numbrix(Problem):
     # TODO: outros metodos da classe
 
 
+###################################################################################################
+#                                             MAIN                                                #
+###################################################################################################
 if __name__ == "__main__":
     # TODO:
     # Ler o ficheiro de input de sys.argv[1],
     board = Board.parse_instance(sys.argv[1])
+    print(Board.boardSize)
     print(board)
-    number = Board.adjacent_vertical_numbers(board, 1, 3)
-    print(number)
+
+    vertical_numbers = Board.adjacent_vertical_numbers(board, 2, 3)
+    print(vertical_numbers)
+
+    horizontal_numbers = Board.adjacent_horizontal_numbers(board, 3, 3)
+    print(horizontal_numbers)
     # Usar uma técnica de procura para resolver a instância,
+
     # Retirar a solução a partir do nó resultante,
     # Imprimir para o standard output no formato indicado.
     pass
